7bbac981ade466405d3746532008fccbf3c5fa65
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-11-28 22:27:39 -0800

    🚧 Add threshold

--- src/logistic_semis.R
+++ src/logistic_semis.R
@@ -10,3 +10,8 @@ all <- glm(semifinal ~ ., data=semis, family="binomial")
 both <- step(intercept_only, direction='both', scope=formula(all), trace=0)
 
 semis$prob_semis <- predict(both, newdata = semis, type = "response")
+
+semis <- semis %>%
+  mutate(pred_semis = ifelse(prob_semis > treshold, TRUE, FALSE))
+test_roc = roc(semis$semifinal ~ semis$prob_semis)
+treshold <- coords(test_roc, "best")$threshold
