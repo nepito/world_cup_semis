15423bb46ebda0fba2907af82dd23f8129f2e2e8
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-12-04 21:36:13 -0800

    🎨 Format with Styler

--- src/logistic_semis.R
+++ src/logistic_semis.R
@@ -3,15 +3,15 @@ path_data <- "tests/data/semis.csv"
 semis <- read_csv(path_data, show_col_types = FALSE) %>%
   select(-c(mundial, equipo))
 
-intercept_only <- glm(semifinal ~ 1, data=semis, family="binomial")
+intercept_only <- glm(semifinal ~ 1, data = semis, family = "binomial")
 
-all <- glm(semifinal ~ ., data=semis, family="binomial")
+all <- glm(semifinal ~ ., data = semis, family = "binomial")
 
-both <- step(intercept_only, direction='both', scope=formula(all), trace=0)
+both <- step(intercept_only, direction = "both", scope = formula(all), trace = 0)
 
 semis$prob_semis <- predict(both, newdata = semis, type = "response")
 
 semis <- semis %>%
   mutate(pred_semis = ifelse(prob_semis > treshold, TRUE, FALSE))
-test_roc = roc(semis$semifinal ~ semis$prob_semis)
+test_roc <- roc(semis$semifinal ~ semis$prob_semis)
 treshold <- coords(test_roc, "best")$threshold
