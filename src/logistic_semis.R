3e242d99d1e12d57cb8d0b2e1a541fd1bfc1753e
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-12-05 22:40:41 -0800

    🧐 Calculate confusion matrix

--- src/logistic_semis.R
+++ src/logistic_semis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
+library(wcSemis)
 path_data <- "tests/data/semis.csv"
-semis <- read_csv(path_data, show_col_types = FALSE) %>%
+semis <- wcSemis::read_csv(path_data) %>%
   select(-c(mundial, equipo))
 
 intercept_only <- glm(semifinal ~ 1, data = semis, family = "binomial")
@@ -9,9 +10,19 @@ all <- glm(semifinal ~ ., data = semis, family = "binomial")
 
 both <- step(intercept_only, direction = "both", scope = formula(all), trace = 0)
 
-semis$prob_semis <- predict(both, newdata = semis, type = "response")
+path_to_solution <- "tests/data/to_test.csv"
+to_solution <- wcSemis::read_csv(path_to_solution)
+to_solution$prob_semis <- predict(both, newdata = to_solution, type = "response")
+to_solution <- to_solution %>%
+  arrange(-prob_semis) %>%
+  select(1) %>%
+  mutate(semis = c(rep(TRUE,4), rep(FALSE, 12))) %>%
+  arrange(id)
 
+treshold <- 0.5
 semis <- semis %>%
   mutate(pred_semis = ifelse(prob_semis > treshold, TRUE, FALSE))
-test_roc <- roc(semis$semifinal ~ semis$prob_semis)
+test_roc <- pROC::roc(semis$semifinal ~ semis$prob_semis)
 treshold <- coords(test_roc, "best")$threshold
+
+caret::confusionMatrix(factor(solution_mexico_86$semifinal), factor(to_solution$semis))
